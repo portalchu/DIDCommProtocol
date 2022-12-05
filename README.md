4e293e2b50c8a9115dcb42d2b0e74967dffc2bd6
portalchu <56888404+portalchu@users.noreply.github.com>
2022-12-06 02:02:13 +0900

    20221206_프로잭트초기화

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DIDCommProtocol
+
+    mvn clean install
+    mvn exec:java -Dexec.mainClass="org.example.Main"
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int num;
+
+        try {
+            while (true)
+            {
+                System.out.println("1 : 서버 실행");
+                System.out.println("2 : 클라이언트 실행");
+                System.out.println("0 : 종료");
+
+                System.out.println("번호 입력 : ");
+                num = sc.nextInt();
+
+                switch (num) {
+                    case 1:
+                        Server.server();
+                        break;
+                    case 2:
+                        Client.client();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        System.out.println("잘못된 입력 값");
+                        break;
+                }
+
+            }
+
+        } catch (Exception e) {
+            System.out.println("Exception : " + e);
+        }
+
+    }
+}
